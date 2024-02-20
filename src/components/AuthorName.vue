4cdc0de317106e9a409200f4bc76cc947b8698d5
Jazmari Gonzalez <jazmari.gonzalez@outlook.com>
2024-02-20 13:36:06 -0600

    ⭐Feat: Author Name Creat the AuthorName component

---
+++ src/components/AuthorName.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="div">
+        <h1> Athor Name</h1>
+        <p>
+            <a target = "blank" href="https://github.com/JazmariGonzalez">
+                Jazmari Gonzalez
+            </a>    
+        </p>
+
+    </div>
+</template>
