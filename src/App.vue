0bd44f03bc315d508858892dad1152ae5e80005c
Jazmari Gonzalez <jazmari.gonzalez@outlook.com>
2024-03-07 12:01:41 -0600

    ⭐FEAT: Checkboxes Bind checkboxes to newItemPriority

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <script setup>
-// Importando funcion
-// para crear referencias reactivas
-import { ref } from 'vue'
-// Creando una referencia reactiva
-// de tipo string
-const header = ref (' App lista de compras');
-//const bag = ref('<i class="material-icons shopping-cart-icon">local_mall</i>')
-const shoppingIcon = ref('material-icons shopping-cart-icon');
-//Creando una referencia reactiva
-//para almacenar el valor de la lista
-const items = ref([
-{id: 0, label:'Leche'},
-{id: 2, label:'Arroz'},
-{id: 3, label:'Carne'},
-{id: 4, label:'Pan'},
-{id: 5, label:'Huevos'}
-]);
+ 
+// Importando funcion para crear referencias reactivas ctrl+k+c
+import{ref} from 'vue'
+// Creando una referencia reactiva de tipo string
+const header = ref(' App lista de compras');
+const shoppingIcon= ref('material-icons shopping-cart-icon');
+// Creando una referencia reactiva para almacenar el valor de la lista
+const items= ref([
+  {id: 0,label:'Leche'},
+  {id: 1,label:'Arroz'},
+  {id: 2,label:'Carne'},
+  {id: 3, label:'Pan'},
+  {id: 4,label:'Huevo'}]);
+ 
 const newItem = ref('');
+const newItemPriority = ref('low');
 </script>
  
 <template>
-   <h1> <i :class="shoppingIcon">local_mall</i> <span v-html="bag"></span> {{ header }} </h1>
-   <input v-model="newItem" placeholder="Agregar Articulo">
-   <ul>
-    <li v-for="({id,label}, i) in items" v-bind:key="id">⭐ {{ i }} {{ label }}</li>
-   </ul>
+  <h1><i :class="shoppingIcon">local_mall</i>{{ header }}</h1>
+  <input v-model="newItem" type="text" placeholder="Agregar Articulos">
+  <!-- Radio Buttons -->
+  <label><input type="radio" v-model="newItemPriority" value="low">Baja</label>
+  <label><input type="radio" v-model="newItemPriority" value="low">Alta</label>
+  {{ newItemPriority === 'low' ?'🧊' : '🔥'}}
+  <ul>
+   <li v-for="item in items" v-bind:key="item.id">⭐ {{item.label}} </li>
+  </ul>
 </template>
+ 
 <style scoped>
+.shopping-cart-icon{font-size: 2rem;}
 </style>
