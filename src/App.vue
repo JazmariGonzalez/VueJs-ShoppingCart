0b850044afe19d7e941eb8c0d31e197d17f31fc6
Jazmari Gonzalez <jazmari.gonzalez@outlook.com>
2024-03-06 20:34:31 -0600

    ⭐Feat: App.vue Add input text for input new items

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <script setup>
- 
-// Importando funcion para crear referencias reactivas ctrl+k+c
-import{ref} from 'vue'
-// Creando una referencia reactiva de tipo string
-const header = ref('App lista de compras');
-const shoppingIcon= ref('material-icons shopping-cart-icon');
-// Creando una referencia reactiva para almacenar el valor de la lista
-const items= ref([
-  {id: 0,label:'Leche'},
-  {id: 1,label:'Arroz'},
-  {id: 2,label:'Carne'},
-  {id: 3,label:'Pan'},
-  {id: 4,label:'Huevo'}]);
- 
+// Importando funcion
+// para crear referencias reactivas
+import { ref } from 'vue'
+// Creando una referencia reactiva
+// de tipo string
+const header = ref (' App lista de compras');
+//const bag = ref('<i class="material-icons shopping-cart-icon">local_mall</i>')
+const shoppingIcon = ref('material-icons shopping-cart-icon');
+//Creando una referencia reactiva
+//para almacenar el valor de la lista
+const items = ref([
+{id: 0, label:'Leche'},
+{id: 2, label:'Arroz'},
+{id: 3, label:'Carne'},
+{id: 4, label:'Pan'},
+{id: 5, label:'Huevos'}
+]);
+const newItem = ref('');
 </script>
  
 <template>
-  <h1><i :class="shoppingIcon">local_mall</i> {{ header }} </h1>
-  <ul>
-   <li v-for="item in items" v-bind:key="item.id">⭐ {{item.label}}</li>
-  </ul>
+   <h1> <i :class="shoppingIcon">local_mall</i> <span v-html="bag"></span> {{ header }} </h1>
+   <input v-model="newItem" placeholder="Agregar Articulo">
+   <ul>
+    <li v-for="({id,label}, i) in items" v-bind:key="id">⭐ {{ i }} {{ label }}</li>
+   </ul>
 </template>
- 
 <style scoped>
-.shopping-cart-icon{font-size: 2rem;}
 </style>
