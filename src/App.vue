c7ab47f3408013bfb23378f21ba946e08792ffab
Jazmari Gonzalez <jazmari.gonzalez@outlook.com>
2024-03-14 12:55:30 -0600

    ⭐Conditional Rendering

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,51 @@
-<script setup>
- 
-// Importando funcion para crear referencias reactivas ctrl+k+c
-import{ref} from 'vue'
-// Creando una referencia reactiva de tipo string
-const header = ref(' App lista de compras');
-const shoppingIcon= ref('material-icons shopping-cart-icon');
-// Creando una referencia reactiva para almacenar el valor de la lista
-const items= ref([
-  {id: 0,label:'Leche'},
-  {id: 1,label:'Arroz'},
-  {id: 2,label:'Carne'},
-  {id: 3, label:'Pan'},
-  {id: 4,label:'Huevo'}]);
- 
-const newItem = ref('');
-const newItemHighPriority = ref(false);
-//Metodos 
-const saveItems = ( ) => { 
-  //Agregar un nuevo elemento a la lista 
-  //proveniente de la caja de texto
-items.value.push ({id:items.value.length, label:newItem.value})
-//Borramos el contenido de la caja de texto 
-newItem.value = "";
-}; 
-</script>
- 
 <template>
-  <h1><i :class="shoppingIcon">local_mall</i>{{ header }}</h1>
-  <form 
-  v-on:submit.prevent="saveItems" 
-  class="add-item form">
-    <input 
-    v-model="newItem" type="text" 
-    placeholder="Agregar Articulos">
+  <!-- Header -->
+  <div class="header">
+    <h1>
+      <i :class="shoppingIcon">local_mall</i>{{ header }}
+    </h1>
+    <button v-if="!showForm" class="btn btn-primary" @click="showForm = true">Agregar Articulo</button>
+    <button v-if="showForm" class="btn" @click="cancelAddItem">Cancelar</button>
+  </div>
+  <!-- Formulario -->
+  <form v-if="showForm" v-on ="saveItems" class="add-item form">
+    <input v-model="newItem" type="text" placeholder="Agregar Articulos">
     <!-- Checkbox -->
     <label>
-      <input type="checkbox" v-moldel="newItemHighPriority"> 
+      <input type="checkbox" v-model="newItemHighPriority"> 
       Alta Prioridad 
     </label>
     <!-- Boton -->
-    <button tyclass="btn btn-primary">
-    Agregar Articulo 
-    </button>
+    <button class="btn btn-primary">Agregar Articulo</button>
   </form>
+  <!-- Entrega de lista -->
   <ul>
-   <li v-for="{id,label} in items" v-bind:key="id">⭐ {{label}} </li>
+    <li v-for="{ id, label } in items" :key="id">⭐ {{ label }} </li>
   </ul>
+  <!-- Mensaje condicional -->
+  <p v-if="items.length === 0">🥀 No hay elementos en la lista 🥀</p>
 </template>
- 
+
+<script setup>
+import { ref } from 'vue'
+
+const header = ref(' App lista de compras');
+const shoppingIcon = ref('material-icons shopping-cart-icon');
+const items = ref([]);
+const newItem = ref('');
+const newItemHighPriority = ref(false);
+const showForm = ref(false);
+
+const saveItems = () => {
+  items.value.push({ id: items.value.length, label: newItem.value });
+  newItem.value = "";
+};
+
+const cancelAddItem = () => {
+  showForm.value = false;
+};
+</script>
+
 <style scoped>
-.shopping-cart-icon{font-size: 2rem;}
+.shopping-cart-icon { font-size: 2rem; }
 </style>
